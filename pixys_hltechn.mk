d64ba53f7f9bc6a057d40a5e00369dbf4178656a
Jviix <33033405+Jviix@users.noreply.github.com>
2018-09-15 18:23:26 +0200

    Update and rename aosp_hltechn.mk to pixys_hltechn.mk

--- pixys_hltechn.mk
+++ pixys_hltechn.mk
@@ -14,15 +14,14 @@
 # limitations under the License.
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/hltechn/full_hlte.mk)
 
 PRODUCT_DEVICE := hltechn
-PRODUCT_NAME := aosp_hltechn
+PRODUCT_NAME := pixys_hltechn
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_ARCH := arm
 TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm
