fb0f03b08581da8661f42ed62040e4481eb35c09
Jviix <jviix8@gmail.com>
2018-10-06 17:45:34 +0000

    Lineage -> Pixys

--- pixys_hltechn.mk
+++ pixys_hltechn.mk
@@ -14,9 +14,13 @@
 # limitations under the License.
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/hltechn/full_hlte.mk)
 
 PRODUCT_DEVICE := hltechn
 PRODUCT_NAME := lineage_hltechn
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
